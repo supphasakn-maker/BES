150e954cea93cbc779661c3e461333f10bd12ffb
supphasak <supphasak.n@gmail.com>
2026-01-07 10:07:54 +0700

    new

--- hesk/inc/loader.inc.php
+++
@@ -1,7 +0,0 @@
-<!--Installed Language Changes Add Loader-->
-<div id="overlay_loader">
-    <div class="main-spinner">
-        <span class="sub-spinner"></span>
-    </div>
-</div>
-<!--Installed Language Changes Add Loader-->
