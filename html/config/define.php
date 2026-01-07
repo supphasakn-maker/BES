5cd4f23196f2dcdcf4114e3f25b2d9284469038c
supphasak <supphasak.n@gmail.com>
2026-01-07 12:24:46 +0700

    remove_db

---
+++ html/config/define.php
@@ -0,0 +1,28 @@
+<?php
+	define("PHP_TIMEZONE","Asia/Bangkok");
+	define("PHP_DATEFORMAT","Asia/Bangkok");
+	
+	define("DB_NAME","erp_main");
+	define("DB_USER","root");
+	define("DB_PASS","M@xnight007");
+	//define("DB_SERVER","172.18.0.2");
+	define("DB_SERVER","localhost");
+	//define("DB_SERVER","db.mysql");
+	
+	define("DEBUG_MODE",true);
+	
+	define("DEFAULT_TIMEZONE","Asia/Bangkok");
+	define("DEFAULT_LANGUAGE","en");
+	
+	define("DEFAULT_THEME","default");
+	define("SERVER_MARKET","oceanos.aquawams.com");
+	
+	define("ORG_NAME","BES (Bowins Enterprise System 4.0)");
+	define("ORG_WEBSITE","https://www.bowinsilvers.com");
+	define("SYSTEM_NAME","BES 4.0");
+	
+	define("GOOGLE_CLIENT_ID","410506135773-g2irchhj0gpjhp422ccfmj90ntbsajuk.apps.googleusercontent.com");
+	
+	date_default_timezone_set("Asia/Bangkok");
+	
+?>
